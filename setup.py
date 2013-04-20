9a7069c2d8edc1de41b0fd6e2c18eee4f5607852
Preston Timmons <prestontimmons@gfa.org>
2013-04-20 21:06:42 +0000

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ https://github.com/prestontimmons/django-dslforms
 setup(
     name="django-dslforms",
     version="0.9.0",
+    author="Preston Timmons",
+    author_email="prestontimmons@gmail.com",
+    url="https://github.com/prestontimmons/django-dslforms",
     description="Create simple Django forms with a mini-language in your templates.",
     long_description=DESCRIPTION,
     packages=find_packages(),
